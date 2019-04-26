b83980c6ff52b3c83c12a992e7cd7999098c1e2f
Franck Dakia <dakiafranck@gmail.com>
2019-04-26 16:48:18 +0000

    initial commit

---
+++ src/SoauthConfiguration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bow\Soauth;
+
+use Bow\Configuration\Loader;
+use Bow\Configuration\Configuration;
+
+class SoauthConfiguration extends Configuration
+{
+	/**
+	 * Create the configuration
+	 * 
+	 * @param Loader $config
+	 * @return mixed
+	 */
+	public function create(Loader $config)
+	{
+
+	}
+
+	/**
+	 * Launch configuration
+	 * 
+	 * @return mixed
+	 */
+	public function run()
+	{
+
+	}
+}
